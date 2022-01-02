43513c43b77963d42a0d219c5a3edd55641b942e
bruderherz <progabdou@gmail.com>
2022-01-02 12:58:00 +0100

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import profileimage from './img/abdou.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>My react App</h1>
+    <h2>My react app</h2>
+     <img src={profileimage} alt="profile-image" />
+    </div>
+  );
+}
+
+export default App;
